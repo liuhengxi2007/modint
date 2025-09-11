2b76fb319226dd801545f350b43114b35ceb10e4
liuhengxi <2019891597@qq.com>
2025-09-11 22:35:57 +0800

    add pow inv / /=

--- modint.cpp
+++ modint.cpp
@@ -1,6 +1,7 @@
 #include<cstdio>
 #include<algorithm>
 #include<array>
+#include<stdexcept>
 typedef unsigned uint;
 typedef unsigned long long ull;
 typedef __uint128_t u128;
@@ -9,6 +10,7 @@ namespace modint
 {
 	using namespace std;
 	constexpr ull INV=-1ull/MOD;
+	constexpr ull qpow(ull a,uint b,ull c=1){for(;b;b>>=1,(a*=a)%=MOD)if(b&1)(c*=a)%=MOD;return (int)c;}
 	constexpr ull div(ull a){return a/MOD+!!(a%MOD);}
 	constexpr ull width(array<ull,2> a){return div(a[0])+div(a[1]);}
 	constexpr bool fit(array<u128,2> a)
@@ -57,12 +59,20 @@ namespace modint
 		}
 		return {{(ull)c[0],(ull)c[1]},t};
 	}
+
+	template<uint A>struct mint_const;
+	template<int O,typename A,typename B>struct mint_expr;
+	struct mint;
+
 	template<uint A>
 	struct mint_const
 	{
-		static constexpr bool is_mint_expr=true;
+		static constexpr bool is_mint_expr=A<MOD;
 		static constexpr array<ull,2> V{A,0};
 		static constexpr uint a=A;
+		static constexpr uint to_int(){return a;}
+		template<uint B>static constexpr mint_const<qpow(A,B)> pow(){return decltype(mint_const<A>::pow<B>())();}
+		static constexpr mint_const<A?qpow(A,MOD-2):-1> inv(){return decltype(mint_const<A>::inv())();}
 	};
 	namespace literal
 	{
@@ -75,23 +85,30 @@ namespace modint
 		template<uint s>struct parser<s>{static constexpr uint v=s;};
 		template<char... c>constexpr auto operator""_m(){return mint_const<parser<0,c...>::v>();}
 	}
-	struct mint;
+
 	template<int O,typename A,typename B>
 	struct mint_expr
 	{
 		static constexpr bool is_mint_expr=A::is_mint_expr&&B::is_mint_expr;
 		static constexpr array<ull,2> V=get_range(O,A::V,B::V).first;
 		enable_if_t<is_mint_expr,ull> a;
-		uint to_mint_uint()
+		constexpr uint to_mint_uint()
 		{
 			constexpr ull d0=div(V[0]),d1=div(V[1]);
 			if constexpr(d0+d1<=2)return (uint)(a+d1*MOD);
 			if constexpr(d0+d1<=4)return (uint)csub<V[0],V[1]>(a);
 			return (uint)barrett<V[0],V[1]>(a);
 		}
-		mint to_mint();
+		constexpr mint to_mint();
+		constexpr uint to_int()
+		{
+			constexpr ull d1=div(V[1]);
+			return (uint)((a+d1*MOD)%MOD);
+		}
+		constexpr mint_expr<3,mint,mint> pow(uint b);
+		constexpr mint_expr<3,mint,mint> inv();
 	};
-	template<int O,typename A,typename B>void prepare(A &a,B &b)
+	template<int O,typename A,typename B>constexpr void prepare(A &a,B &b)
 	{
 		constexpr array<int,2> t=get_range(O,A::V,B::V).second;
 		if constexpr(t[0]==1)a.a=csub<A::V[0],A::V[1]>(a.a);
@@ -99,16 +116,16 @@ namespace modint
 		if constexpr(t[1]==1)b.a=csub<B::V[0],B::V[1]>(b.a);
 		if constexpr(t[1]==2)b.a=barrett<B::V[0],B::V[1]>(b.a);
 	}
-	template<typename A>enable_if_t<A::is_mint_expr,A> operator+(A a)
+	template<typename A>constexpr enable_if_t<A::is_mint_expr,A> operator+(A a)
 	{
 		return a;
 	}
-	template<typename A>enable_if_t<A::is_mint_expr,mint_expr<0,A,mint_const<0>>> operator-(A a)
+	template<typename A>constexpr enable_if_t<A::is_mint_expr,mint_expr<0,A,mint_const<0>>> operator-(A a)
 	{
 		return {-(ull)a.a};
 	}
 #define MODINT_DEFINE_OP(n_op,op) \
-	template<typename A,typename B>enable_if_t<A::is_mint_expr&&B::is_mint_expr,mint_expr<n_op,A,B>> \
+	template<typename A,typename B>constexpr enable_if_t<A::is_mint_expr&&B::is_mint_expr,mint_expr<n_op,A,B>> \
 		operator op(A a,B b) \
 	{ \
 		prepare<n_op,A,B>(a,b); \
@@ -118,18 +135,29 @@ namespace modint
 	MODINT_DEFINE_OP(2,-)
 	MODINT_DEFINE_OP(3,*)
 #undef MODINT_DEFINE_OP
+	template<typename A,typename B>
+		constexpr enable_if_t<A::is_mint_expr&&B::is_mint_expr,mint_expr<3,A,decltype(declval<B>().inv())>>
+		operator/(A a,B b)
+	{
+		return a*b.inv();
+	}
+
 	struct mint
 	{
 		static constexpr bool is_mint_expr=true;
 		static constexpr array<ull,2> V{2*MOD,0};
+		static mint raw(uint a)
+		{
+			mint x;x.a=a;
+			return x;
+		}
 		uint a;
-		mint():a(){}
-		template<uint v>mint(mint_const<v>):a(v){}
-		template<int O,typename A,typename B>mint(mint_expr<O,A,B> b):a(b.to_mint_uint()){}
-		uint raw()const{return a;}
-		uint to_int()const{return a==2*MOD?0:min(a,a-MOD);}
+		mint()=default;
+		template<uint v>constexpr mint(mint_const<v>):a(v){}
+		template<int O,typename A,typename B>constexpr mint(mint_expr<O,A,B> b):a(b.to_mint_uint()){}
+		constexpr uint to_int()const{return a==2*MOD?0:min(a,a-MOD);}
 #define MODINT_DEFINE_OP(n_op,op) \
-		template<typename B>enable_if_t<B::is_mint_expr,mint&> \
+		template<typename B>constexpr enable_if_t<B::is_mint_expr,mint&> \
 			operator op##=(B b) \
 		{ \
 			*this=*this op b; \
@@ -139,9 +167,30 @@ namespace modint
 		MODINT_DEFINE_OP(2,-)
 		MODINT_DEFINE_OP(3,*)
 #undef MODINT_DEFINE_OP
+		constexpr mint_expr<3,mint,mint> pow(uint b)
+		{
+			mint x=*this,y=*this;y.a=1;
+			if(!b)return y*y;
+			for(;b>1;b>>=1)
+			{
+				if(b&1)y*=x;
+				x*=x;
+			}
+			return x*y;
+		}
+		constexpr mint_expr<3,mint,mint> inv()
+		{
+			if(!a)throw runtime_error("division by zero");
+			return pow(MOD-2);
+		}
 	};
+
 	template<int O,typename A,typename B>
-	mint mint_expr<O,A,B>::to_mint(){return mint(*this);}
+	constexpr mint mint_expr<O,A,B>::to_mint(){return mint(*this);}
+	template<int O,typename A,typename B>
+	constexpr mint_expr<3,mint,mint> mint_expr<O,A,B>::pow(uint b){return mint(*this).pow(b);}
+	template<int O,typename A,typename B>
+	constexpr mint_expr<3,mint,mint> mint_expr<O,A,B>::inv(){return mint(*this).inv();}
 }
 using modint::literal::operator""_m;
 using modint::mint;
@@ -187,11 +236,13 @@ void test2()
 void test3()
 {
 	mint a=1_m;
-	for(int i=0;i<10*N;++i)a=a*6_m*3_m*2_m;
+	for(int i=0;i<10*N;++i)a*=6_m*3_m*2_m;
 	printf("%d\n",a.to_int());
 }
 int main()
 {
-	test1();
+	mint a=1_m/2_m*3_m;
+	mint b=a+1000_m;
+	printf("%d\n",b.to_int());
 	return 0;
 }
